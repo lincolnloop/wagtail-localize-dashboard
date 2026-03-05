08fd1960b575bc4f173e069fc418e28738dcb33d
D Chukhin <6025153+dchukhin@users.noreply.github.com>
2026-03-05 11:21:51 -0500

    update project version to 0.2.0

--- wagtail_localize_dashboard/__init__.py
+++ wagtail_localize_dashboard/__init__.py
@@ -9,7 +9,7 @@ Provides cached translation progress tracking with automatic updates.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __author__ = "Lincoln Loop"
 __license__ = "MIT"
 
